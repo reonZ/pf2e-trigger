0b3f421b4b7eb985afe00f5a858dfc8eff4f518b
Idle <651462+reonZ@users.noreply.github.com>
2025-01-09 12:21:29 +0100

    move item getter from ItemSourceValueTriggerNode to ItemSourceValueBlueprintNode

--- src/blueprint/node/value/blueprint-item-source.ts
+++ src/blueprint/node/value/blueprint-item-source.ts
@@ -1,15 +1,26 @@
 import { ItemSourceValueTriggerNode } from "@node/value/item-source";
 import { ValueBlueprintNode } from "./blueprint-value";
+import { ItemPF2e, R, isItemEntry } from "module-helpers";
 
 class ItemSourceValueBlueprintNode extends ValueBlueprintNode {
     get title(): string | null {
-        const item = this.trigger.item;
+        const item = this.item;
         return item?.name || super.title;
     }
 
     get icon(): string | PIXI.Sprite | null {
-        const item = this.trigger.item;
+        const item = this.item;
         return item === null ? "\uf127" : item ? PIXI.Sprite.from(item.img) : super.icon;
+    }
+
+    get item(): ItemPF2e | CompendiumIndexData | undefined | null {
+        const value = this.trigger.getValue("inputs", "uuid");
+        if (!R.isString(value) || !value.trim()) return;
+
+        const item = fromUuidSync<ItemPF2e>(value);
+        if (item instanceof Item && item.sourceId !== value) return null;
+
+        return isItemEntry(item) ? item : null;
     }
 }
 
--- src/node/value/item-source.ts
+++ src/node/value/item-source.ts
@@ -1,6 +1,5 @@
 import { NodeSchema } from "@node/trigger-node";
 import { ValueTriggerNode } from "./value";
-import { ItemPF2e, R, isItemEntry } from "module-helpers";
 
 class ItemSourceValueTriggerNode extends ValueTriggerNode {
     get schema(): NodeSchema {
@@ -8,16 +7,6 @@ class ItemSourceValueTriggerNode extends ValueTriggerNode {
             inputs: [{ key: "uuid", type: "uuid" }],
             outputs: [{ key: "item", type: "item" }],
         };
-    }
-
-    get item(): ItemPF2e | CompendiumIndexData | undefined | null {
-        const value = this.getValue("inputs", "uuid");
-        if (!R.isString(value) || !value.trim()) return;
-
-        const item = fromUuidSync<ItemPF2e>(value);
-        if (item instanceof Item && item.sourceId !== value) return null;
-
-        return isItemEntry(item) ? item : null;
     }
 }
 
