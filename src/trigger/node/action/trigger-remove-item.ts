6978fdf1ac9efabd416d8d28317399e5f163a4cf
Idle <651462+reonZ@users.noreply.github.com>
2025-01-22 16:36:39 +0100

    fix removing item from origin instead of target

--- src/trigger/node/action/trigger-remove-item.ts
+++ src/trigger/node/action/trigger-remove-item.ts
@@ -6,11 +6,10 @@ import { getItemWithSourceId } from "module-helpers";
 class RemoveItemTriggerNode extends TriggerNode<typeof removeItemSchema> {
     protected async _execute(origin: TargetDocuments, options: TriggerExecuteOptions) {
         const item = await this.get("item");
+        if (!item) return;
 
-        if (item) {
-            const exist = getItemWithSourceId(origin.actor, item.uuid);
-            await exist?.delete();
-        }
+        const exist = getItemWithSourceId(options.target.actor, item.uuid);
+        await exist?.delete();
 
         this.send("out", origin, options);
     }
