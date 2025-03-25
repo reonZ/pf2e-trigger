afb9b8dbfec88e30519df8c8e1ec6597c71b8c66
Idle <651462+reonZ@users.noreply.github.com>
2025-03-25 14:48:40 +0100

    add target to the extractables list

--- src/helpers/helpers-extract.ts
+++ src/helpers/helpers-extract.ts
@@ -1,7 +1,7 @@
 import { NODE_ENTRY_VALUE_TYPE } from "data/data-entry";
 import { R } from "module-helpers";
 
-const EXTRACT_TYPES = ["boolean", "number", "text", "list"] as const;
+const EXTRACT_TYPES = ["boolean", "number", "text", "list", "target"] as const;
 
 function extractValueFromDocument(
     document: ClientDocument,
@@ -10,7 +10,21 @@ function extractValueFromDocument(
 ) {
     const cursor = fu.getProperty(document, path);
 
-    if (type === "list") {
+    if (type === "target") {
+        if (cursor instanceof Actor) {
+            return { actor: cursor };
+        }
+
+        const token = cursor instanceof Token ? cursor.document : cursor;
+
+        if (token instanceof TokenDocument) {
+            const actor = token.actor;
+
+            if (actor instanceof Actor) {
+                return { actor, token };
+            }
+        }
+    } else if (type === "list") {
         return R.pipe(
             cursor instanceof Set ? [...cursor] : R.isArray(cursor) ? cursor : [],
             R.filter(R.isString)
