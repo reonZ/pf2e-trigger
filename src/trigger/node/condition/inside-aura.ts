28483622711d5b0f40ccddab34126cc3f3d3c924
Idle <651462+reonZ@users.noreply.github.com>
2025-05-02 20:44:12 +0200

    implement inside-aura condition node

--- src/trigger/node/condition/inside-aura.ts
+++ src/trigger/node/condition/inside-aura.ts
@@ -1,18 +1,29 @@
 import { NodeSchemaOf } from "schema";
 import { TriggerNode } from "../node";
+import { getAurasInMemory } from "hook";
+import { actorsRespectAlliance, ActorTargetAlliance } from "module-helpers";
 
 class InsideAuraTriggerNode extends TriggerNode<InsideAuraSchema> {
     async execute(): Promise<boolean> {
-        // TODO return false if not inside aura
-
-        // for (let i = 0; i < 5; i++) {
-        //     // console.log("iteration", i);
-        //     // this.setVariable("source", i);
-        //     await this.send("true");
-        // }
         const slug = await this.get("slug");
-        if (!slug) {
+        const targets = (await this.get("targets")) as ActorTargetAlliance;
+        const { actor } = (await this.getTarget("target")) ?? {};
+
+        if (!slug || !actor) {
             return this.send("false");
+        }
+
+        const auras = getAurasInMemory(actor).filter(({ data, origin }) => {
+            return data.slug === slug && actorsRespectAlliance(origin.actor, actor, targets);
+        });
+
+        if (!auras.length) {
+            return this.send("false");
+        }
+
+        for (const { origin } of auras) {
+            this.setVariable("source", origin);
+            await this.send("true");
         }
 
         return true;
