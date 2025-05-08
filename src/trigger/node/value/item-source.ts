7b32e166013f3d81989a36fdd2b54cc114ede8ed
Idle <651462+reonZ@users.noreply.github.com>
2025-05-08 17:26:41 +0200

    cache the item in item-source

--- src/trigger/node/value/item-source.ts
+++ src/trigger/node/value/item-source.ts
@@ -3,9 +3,15 @@ import { NodeSchemaOf } from "schema";
 import { TriggerNode } from "trigger";
 
 class ItemSourceTriggerNode extends TriggerNode<NodeSchemaOf<"value", "item-source">> {
-    async query(): Promise<ItemPF2e | undefined> {
+    #cached: Maybe<ItemPF2e>;
+
+    async query(): Promise<Maybe<ItemPF2e>> {
+        if (this.#cached !== undefined) {
+            return this.#cached;
+        }
+
         const uuid = await this.get("uuid");
-        return getItemFromUuid(uuid);
+        return (this.#cached = await getItemFromUuid(uuid));
     }
 }
 
