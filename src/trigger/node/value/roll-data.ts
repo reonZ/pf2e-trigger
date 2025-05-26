fe98a090acdc0355475f26151cfc99847a7292ac
Idle <651462+reonZ@users.noreply.github.com>
2025-05-26 12:30:54 +0200

    update to use splitListString

--- src/trigger/node/value/roll-data.ts
+++ src/trigger/node/value/roll-data.ts
@@ -1,4 +1,4 @@
-import { ActorPF2e, ItemPF2e, splitStr } from "module-helpers";
+import { ActorPF2e, ItemPF2e, splitListString } from "module-helpers";
 import { NodeSchemaOf } from "schema";
 import { TriggerNode, TriggerRollEntry } from "trigger";
 
@@ -6,9 +6,9 @@ class RollDataTriggerNode extends TriggerNode<NodeSchemaOf<"value", "roll-data">
     async query(): Promise<TriggerRollEntry> {
         return {
             item: (await this.get("item")) as ItemPF2e<ActorPF2e>,
-            options: splitStr(await this.get("options")),
+            options: splitListString(await this.get("options")),
             origin: await this.get("origin"),
-            traits: splitStr(await this.get("traits")),
+            traits: splitListString(await this.get("traits")),
         };
     }
 }
